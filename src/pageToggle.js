cc17b2b392b99b5619f276667586b1085c6ad00a
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-04-08 16:08:02 +1000

    Fixed import of separate files on button click

--- src/pageToggle.js
+++ src/pageToggle.js
@@ -1,12 +1,25 @@
+import { home } from './home';
+import { menu } from './menu';
+import { about } from './about';
+
 function pageToggle() {
     const navButtons = document.querySelectorAll(".navButton");
-    const content = document.getElementById("content")
+    const content = document.getElementById("content");
 
     navButtons.forEach(button => {
         button.addEventListener("click", () => {
-            console.log(`You pressed a button: ${button.id}`);
-            content.innerHTML = ''
+            const id = button.id;
+            content.innerHTML = '';
             console.log(content.innerHTML);
+            if (id === 'homeButton') {
+                home();
+            }
+            else if (id === 'menuButton') {
+                menu();
+            }
+            else if (id === 'aboutButton') {
+                about();
+            }
         })
     })
 }
