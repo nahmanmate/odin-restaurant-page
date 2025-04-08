cd6bd0fd0e680524224a590cbacf653069291433
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-04-08 15:02:12 +1000

    Fixed layout inconsistency and improved responsivenes

--- src/header.js
+++ src/header.js
@@ -1,13 +1,14 @@
 // Import the image so webpack can process it
 function header() {
     //declare div#id as constant
-    const content = document.getElementById("content");
+    const body = document.querySelector("body")
 
     // create homepage, image, headline, some text etc.
 
     const hero = document.createElement("div");
+    const content = document.getElementById("content")
     hero.classList = "hero";
-    content.append(hero);
+    body.insertBefore(hero, content);
 
     const heading = document.createElement("h1");
     heading.textContent = "Little Mario's Pizzeria"
